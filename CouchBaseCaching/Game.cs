a823c48f81dfc15e658c16acc10144be9eb716b6
Hugin Antony <HuginAntony@gmail.com>
2019-07-17 09:25:59 +0200

    Adding new files

---
+++ CouchBaseCaching/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CouchBaseCaching
+{
+    public class Game
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
