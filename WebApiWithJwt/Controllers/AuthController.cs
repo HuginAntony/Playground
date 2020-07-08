949c64f41d8e54643a792f631fe9deaa41a2c83f
Hugin Antony <huginantony@gmail.com>
2020-07-08 15:26:01 +0200

    Adding WebApiWithJwt project

---
+++ WebApiWithJwt/Controllers/AuthController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebApiWithJwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public AuthController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        [Authorize(Policy = Policies.Admin)]
+        public async Task<IActionResult> GetInfo()
+        {
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(User user)
+        {
+            var token = GenerateJwtToken(user);
+
+            return Ok(new
+            {
+                token = token, user
+            });
+        }
+
+
+        public string GenerateJwtToken(User userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:SecretKey"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userInfo.UserName),
+                new Claim("userName", userInfo.UserName),
+                new Claim("role",userInfo.UserRole),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            //var tokenDescriptor = new SecurityTokenDescriptor
+            //{
+            //    Subject = new ClaimsIdentity(claims),
+            //    Claims = claims,
+            //    Issuer = _config["Jwt:Issuer"],
+            //    Audience = _config["Jwt:Audience"],
+            //    Expires = DateTime.Now.AddDays(1),
+            //    SigningCredentials = credentials,
+            //};
+
+            //var tokenHandler = new JwtSecurityTokenHandler();
+
+            //var token = tokenHandler.CreateToken(tokenDescriptor);
+            //return tokenHandler.WriteToken(token);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Audience"], claims,
+            expires: DateTime.Now.AddMinutes(30), signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+    }
+
+    public class User
+    {
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public string UserRole { get; set; }
+    }
+
+    public class Policies
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+        
+        public static AuthorizationPolicy AdminPolicy()
+        {
+            return new AuthorizationPolicyBuilder().RequireAuthenticatedUser().RequireRole(Admin).Build();
+        }
+
+        public static AuthorizationPolicy UserPolicy()
+        {
+            return new AuthorizationPolicyBuilder().RequireAuthenticatedUser().RequireRole(User).Build();
+        }
+    }
+}
